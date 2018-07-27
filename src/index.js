cd9016fa575345721235eeca539f2488b2a89219
poprobertdaniel <poprobertdaniel22@gmail.com>
2018-07-27 21:54:55 +0300

    working qr scanner

--- src/index.js
+++ src/index.js
@@ -1,23 +1,67 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Linking, TouchableOpacity } from 'react-native';
+import Permissions from 'react-native-permissions';
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 export default class App extends React.Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {
+			cameraPermission: false,
+			data: null
+		}
+	}
+
+	componentDidMount() {
+		Permissions.request('camera').then(response => {
+      this.setState({ cameraPermission: response })
+    })
+	}
+
+	onSuccess = e => {
+   this.setState({
+		 data: e.data
+	 })
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <QRCodeScanner
+        onRead={this.onSuccess}
+        topContent={
+          <Text style={styles.centerText}>
+						data fain: {this.state.data}
+          </Text>
+        }
+        bottomContent={
+          <TouchableOpacity style={styles.buttonTouchable}>
+            <Text style={styles.buttonText}>OK. Got it!</Text>
+          </TouchableOpacity>
+				}
+				reactivate={true}
+				reactivateTimeout={2000}
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  centerText: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
   },
 });
